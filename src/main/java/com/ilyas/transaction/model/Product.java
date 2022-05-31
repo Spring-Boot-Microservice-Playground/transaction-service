5237ceb1d9cf2c148acff286e3dd600e9b4eca2e
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2022-05-31 11:47:24 +0800

    make transaction's product response have an id

--- src/main/java/com/ilyas/transaction/model/Product.java
+++ src/main/java/com/ilyas/transaction/model/Product.java
@@ -1,11 +1,15 @@
 package com.ilyas.transaction.model;
 
+import org.springframework.data.mongodb.core.mapping.Field;
+
 public class Product {
+    @Field("id")
     private String id;
     private String name;
     private Integer amount;
     private Integer price;
-    public Product(String name, Integer amount, Integer price) {
+    public Product(String id, String name, Integer amount, Integer price) {
+        this.id = id;
         this.name = name;
         this.amount = amount;
         this.price = price;
